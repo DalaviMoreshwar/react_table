11d5e5d3e287b78e5542116d8a5eb9023c86ca1e
Moreshwar Dalvi <moreshwardlv@gmail.com>
2021-03-06 16:58:24 +0530

    implemented sorting on table

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BasicTable } from "./Components/BasicTable";
+import { SortingTable } from "./Components/SortingTable";
 import "./styles.css";
 
 export default function App() {
@@ -6,6 +7,7 @@ export default function App() {
     <div className="App">
       <h1>React Table</h1>
       <BasicTable />
+      <SortingTable />
     </div>
   );
 }
